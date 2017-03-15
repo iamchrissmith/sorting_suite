6fbe9282b6e82f3689883ea9e316f7d6bee0067d
Chris Smith <42@chrisryansmith.com>
2017-03-15 12:54:50 -0600

    add instruction's test case

--- insert_sort_test.rb
+++ insert_sort_test.rb
@@ -10,11 +10,16 @@ class InsertionSortTest < MiniTest::Test
     assert sorter
   end
 
-  def test_insert_sort_works
+  def test_insert_sort_works_for_random_alphabet
     sorter = InsertionSort.new
     expected = ('a'..'z').to_a
     test_letters = expected.shuffle
     assert_equal expected, sorter.sort(test_letters)
   end
-
+  def test_insert_sort_works_for_assignment_set
+    sorter = InsertionSort.new
+    expected = ["a", "b", "c", "d"]
+    test_letters = ["d", "b", "a", "c"]
+    assert_equal expected, sorter.sort(test_letters)
+  end
 end
