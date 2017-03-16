8f401a1b47bcb50d425cdef99759f5e0fdb42443
Chris Smith <42@chrisryansmith.com>
2017-03-16 15:01:00 -0600

    merge_sort and merge_sort_tests completed

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,3 +3,58 @@ require 'pry'
 # sorter = MergeSort.new
 # sorter.sort(["d", "b", "a", "c"])
 # => ["a", "b", "c", "d"]
+
+class MergeSort
+  def compare_arrays(first, second)
+    combined = []
+    until first.empty? || second.empty?
+      if ( first[0] > second[0])
+        combined << second[0]
+        second.shift
+      else
+        combined << first[0]
+        first.shift
+      end
+    end
+    until first.empty?
+      combined << first[0]
+      first.shift
+    end
+    until second.empty?
+      combined << second[0]
+      second.shift
+    end
+    combined
+  end
+
+  def sort(unsorted)
+    if unsorted.length < 2
+      return unsorted
+    end
+    split = []
+    unsorted.each_slice(1) {|a| split << a}
+    until split.empty?
+      iterations = (split.length / 2.0).ceil
+      sorted = []
+      iterations.times do
+        if split[1].nil?
+          odd = compare_arrays(split[0], sorted.last)
+          sorted.pop
+          sorted << odd
+          split.pop
+        else
+          sorted << compare_arrays(split[0], split[1])
+          split = split.drop(2)
+        end
+      end
+      unless sorted.length == 1 && split.empty?
+        split = sorted
+      end
+    end
+    sorted[0]
+  end
+
+end
+
+# sorter = MergeSort.new
+# p sorter.sort([4,1,5,6,2,7,20,25,100,5,5,25,500,3,19])
---
+++ merge_sort_test.rb
@@ -0,0 +1,34 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require_relative 'merge_sort'
+
+class MergeSortTest < MiniTest::Test
+  def test_merge_sort_exists
+    sorter = MergeSort.new
+    assert sorter
+  end
+
+  def test_merge_sort_returns_array
+    sorter = MergeSort.new
+    assert_kind_of Array, sorter.sort([])
+    test_letters = ["d", "b", "a", "c"]
+    assert_kind_of Array, sorter.sort(test_letters)
+  end
+
+  def test_merge_sort_works_for_random_alphabet
+    sorter = MergeSort.new
+    expected = ('a'..'z').to_a
+    test_letters = expected.shuffle
+    assert_equal expected, sorter.sort(test_letters)
+  end
+
+  def test_merge_sort_works_for_assignment_set
+    sorter = MergeSort.new
+    expected = ["a", "b", "c", "d"]
+    test_letters = ["d", "b", "a", "c"]
+    assert_equal expected, sorter.sort(test_letters)
+  end
+
+end
